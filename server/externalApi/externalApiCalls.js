0382fd4723b7ad9567a71f317097cbf366f2a198
Marlesli <122844039+Marlesli@users.noreply.github.com>
2025-05-05 00:29:19 +0000

    add alert and currWeather API

--- server/externalApi/externalApiCalls.js
+++ server/externalApi/externalApiCalls.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
  const apiKey = 'YouApiKey'
+ //forecast api
  
  module.exports = {
      forecastApi: async (location, days) => {
@@ -11,8 +12,29 @@ const axios = require('axios');
                  console.log(error);
                  return{ error: "Unable to fetch forecast information." }
              })
-     },
+
+            },
+
+              // current weather 
+    currentWeatherApi: async (location) => {
+        return await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`)
+            .then(response => response.data)
+            .catch(error => {
+                console.log(error);
+                return { error: "Unable to fetch current weather information." };
+            });
+    },
+
+    //Alerts API
+    alertsApi: async (location) => {
+        return await axios.get(`http://api.weatherapi.com/v1/alerts.json?key=${apiKey}&q=${location}&days=1&alerts=yes`)
+            .then(response => response.data.alerts || [])
+            .catch(error => {
+                console.log(error);
+                return { error: "Unable to fetch weather alerts." };
+            });
      // add alert API call hint: change forecast.json to match api wanted 
      // add current API
      // documentation https://www.weatherapi.com/docs/#intro-request
+        }
  }
